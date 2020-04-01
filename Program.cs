0b496ddfe5cc865ab17ef85ba67c382b97789c13
ruchika <ruchika6698@gmail.com>
2020-04-01 15:38:08 +0530

    Added:User Should Fill the Details

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User_Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Filled the Details of User and Register");
+        }
+    }
+}
